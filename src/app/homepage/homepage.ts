2be359a9449e0cb494dd138e5c5263518738a9de
bycfrkn <boyacifrkn@gmail.com>
2025-10-22 23:38:57 +0300

    FİX

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,62 +1,95 @@
-import { Component, OnInit } from '@angular/core';
-import { Ball } from '../model/Ball';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { SharedModule } from '../SharedModule';
+import { Subject } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
+
+export interface Ball {
+  x: number;
+  y: number;
+  radius: number;
+  color: string;
+  velocity: number;
+}
 
 @Component({
   selector: 'app-homepage',
+  standalone: true,
   imports: [SharedModule],
   templateUrl: './homepage.html',
   styleUrl: './homepage.scss'
 })
 export class Homepage implements OnInit {
 
-  gravity: number = 0;
-  width: number = 0;
-  height: number = 0;
+private ballChange=new Subject<Ball>();
 
-  balls: Ball[] =[]
+ ballChange$ = this.ballChange.asObservable();
+
+  notifyBallChange(ball:Ball) {
+  this.ballChange.next(ball);
+  }
+
+  gravity = 0;
+  width = 0;
+  height = 0;
+  animationId?: number;
+  ball: Ball | null = null; 
 
   ngOnInit(): void {
+  if (typeof window !== 'undefined') {
     this.width = window.innerWidth;
     this.height = window.innerHeight;
-    this.gravity = this.height / 3000; 
+    this.gravity = this.height / 1000;
 
-    console.log('Sayfa genişliği:', this.width);
-    console.log('Sayfa yüksekliği:', this.height);
-    console.log('Gravity:', this.gravity);
+    this.ballChange$.subscribe((res) => {
+      this.ball = res;
+    });
+  }
   }
 
   onSceneClick(event: MouseEvent) {
     const x = event.clientX;
     const y = event.clientY;
 
-    const newBall: Ball = {
+
+    this.ball = {
       x,
       y,
-      radius: 50,
+      radius: 30,
       color: this.getRandomColor(),
       velocity: 0
     };
 
-    this.balls.push(newBall);
-    this.animateBall(newBall);
+    this.animateBall();
   }
 
-  animateBall(ball: Ball) {
-    const fall = () => {
-      ball.velocity += this.gravity;     
-      ball.y += ball.velocity;           
+getBall(){
+  return this.ball?.y
+}
 
-      
-      if (ball.y + ball.radius >= this.height) {
-        ball.y = this.height - ball.radius;
-        ball.velocity *= -0.7; 
+
+  animateBall() {
+    if (!this.ball) return;
+
+    const fall = () => {
+      if (!this.ball) return;
+
+      this.ball.velocity += this.gravity;
+      this.ball.y += this.ball.velocity;
+    
+
+      if (this.ball.y + this.ball.radius >= this.height) {
+        this.ball.y = this.height - this.ball.radius;
+        this.notifyBallChange(this.ball)
+         cancelAnimationFrame(this.animationId!);
+      console.log('Animasyon durdu ✅');
+      return;
       }
-      this.balls.push(ball)
-      requestAnimationFrame(fall);
+      this.notifyBallChange(this.ball)
+      
+    this.animationId = requestAnimationFrame(fall);
     };
 
-    requestAnimationFrame(fall);
+  this.animationId =  requestAnimationFrame(fall);
   }
 
   getRandomColor(): string {
